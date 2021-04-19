0870b100b6c46ed13aa7065ab3e46706c7af3fe9
Bharat Gyamlani <golugyamlani@gmail.com>
2021-04-19 18:59:53 +0530

    initial commit, before bloc, before adding database

---
+++ lib/app/sign_in/sign_in_page.dart
@@ -0,0 +1,142 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:time_tracker/app/sign_in/email_sign_in_page.dart';
+import 'package:time_tracker/app/sign_in/sign_in_button.dart';
+import 'package:time_tracker/app/sign_in/social_sign_in_button.dart';
+import 'package:time_tracker/common_widgets/show_exception_alert_dialog.dart';
+import 'package:time_tracker/services/auth.dart';
+
+class SignInPage extends StatefulWidget {
+  @override
+  _SignInPageState createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  bool _isLoading = false;
+
+  void _showSignInError(BuildContext context, Exception exception) {
+    if (exception is FirebaseException &&
+        exception.code == 'ERROR_ABORTED_BY_USER') {
+      return;
+    }
+    showExceptionAlertDialog(
+      context,
+      title: 'Sign in failed',
+      exception: exception,
+    );
+  }
+
+  Future<void> _signInAnonymously(BuildContext context) async {
+    try {
+      setState(() => _isLoading = true);
+      final auth = Provider.of<AuthBase>(context, listen: false);
+      await auth.signInAnonymously();
+    } on Exception catch (e) {
+      _showSignInError(context, e);
+    } finally {
+      setState(() => _isLoading = false);
+    }
+  }
+
+  Future<void> _signInWithGoogle(BuildContext context) async {
+    try {
+      setState(() => _isLoading = true);
+      final auth = Provider.of<AuthBase>(context, listen: false);
+      await auth.signInWithGoogle();
+    } on Exception catch (e) {
+      _showSignInError(context, e);
+    } finally {
+      setState(() => _isLoading = false);
+    }
+  }
+
+  void _signInWithEmail(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        fullscreenDialog: true, //specific to iOS
+        builder: (context) => EmailSignInPage(),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('InviQ'),
+        elevation: 2.0,
+      ),
+      body: _buildContent(context),
+      backgroundColor: Colors.grey[200],
+    );
+  }
+
+  Widget _buildContent(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          SizedBox(
+            height: 50.0,
+            child: _buildHeader(),
+          ),
+          SizedBox(height: 48.0),
+          SocialSignInButton(
+            assetName: 'images/google-logo.png',
+            text: 'Sign in with Google',
+            textColor: Colors.black87,
+            color: Colors.white,
+            onPressed: _isLoading ? null : () => _signInWithGoogle(context),
+          ),
+          // SizedBox(height: 8.0),
+          // SocialSignInButton(
+          //   assetName: 'images/facebook-logo.png',
+          //   text: 'Sign in with Facebook',
+          //   textColor: Colors.white,
+          //   color: Color(0xFF334D92),
+          //   onPressed: () {},
+          // ),
+          SizedBox(height: 8.0),
+          SignInButton(
+            text: 'Sign in with email',
+            textColor: Colors.white,
+            color: Colors.teal[700],
+            onPressed: _isLoading ? null : () => _signInWithEmail(context),
+          ),
+          SizedBox(height: 8.0),
+          Text(
+            'or',
+            style: TextStyle(fontSize: 14.0, color: Colors.black87),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 8.0),
+          SignInButton(
+            text: 'Go anonymous',
+            textColor: Colors.black,
+            color: Colors.lime[300],
+            onPressed: _isLoading ? null : () => _signInAnonymously(context),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildHeader() {
+    if (_isLoading) {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+    return Text(
+      'Sign in',
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        fontSize: 32.0,
+        fontWeight: FontWeight.w600,
+      ),
+    );
+  }
+}
